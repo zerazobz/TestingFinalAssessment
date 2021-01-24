be7e4a370a7bffc670502697959061947055a496
Erick Asto Oblitas <erickastooblitas>
2021-01-24 03:00:54 -0500

    First Unit Test finished

---
+++ MyClasses/MyClasses/FileProcess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClasses
+{
+    public class FileProcess
+    {
+        public bool FileExists(string fileName)
+        {
+            if (String.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentNullException("fileName");
+            }
+
+            return File.Exists(fileName);
+        }
+    }
+}
---
+++ MyClasses/MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExist()
+        {
+            FileProcess fileProcess = new FileProcess();
+            bool fromCall;
+
+            fromCall = fileProcess.FileExists(@"C:\Windows\Regedit.exe");
+
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExist()
+        {
+            FileProcess fileProcess = new FileProcess();
+            bool fromCall;
+
+            fromCall = fileProcess.FileExists(@"C:\Windows\BadFilename.bad");
+
+            Assert.IsFalse(fromCall);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileNameNullOrEmpty_ThrowArgumentException()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists("");
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowArgumentException_UsingTryCatch()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            try
+            {
+                fromCall = fp.FileExists("");
+            }
+            catch (ArgumentNullException)
+            {
+                //The test was a success
+                return;
+            }
+
+            Assert.Fail("Call to FileExists did not throw ArgumentException.");
+        }
+    }
+}
